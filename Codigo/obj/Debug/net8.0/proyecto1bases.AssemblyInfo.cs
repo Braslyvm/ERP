4be1a49fa6fb80069a8a9d34750f139f3b7d6cb2
ELDERPEREZ <colegioempresarial20@gmail.com>
2024-09-22 21:53:55 -0600

    actualizacion

--- Codigo/obj/Debug/net8.0/proyecto1bases.AssemblyInfo.cs
+++ Codigo/obj/Debug/net8.0/proyecto1bases.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("proyecto1bases")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6fc5a8b45b25cf992c3176e6bbefb6f04dc39abf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e11c403adf639541367868eea8ac74d8eeaff6f0")]
 [assembly: System.Reflection.AssemblyProductAttribute("proyecto1bases")]
 [assembly: System.Reflection.AssemblyTitleAttribute("proyecto1bases")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
