155060bcccf5e3c938bbd2a4747bf275824475f6
ELDERPEREZ <colegioempresarial20@gmail.com>
2024-11-14 20:31:31 -0600

    de vistas a fucniones

--- script/Brasly.sql
+++ script/Brasly.sql
@@ -2,27 +2,40 @@ use Planificador_recursos_empresariales
 go
 
 -----------------------------retorna el top 15 tareas -----------------------------------------
-create function cotizaciones.top15_tareas()
+create function cotizaciones.top15_tareas(
+@fecha_inicio date = null,
+    @fecha_fin date = null
+)
 returns table
 as
 return
 (
     select top 15 
         id_tarea,
-        id_cotizacion,
+        ct.id_cotizacion,
         descripcion,
         usuario,
         fecha_inicio,
         fecha_limite,
-        estado
-    from cotizaciones.tareas
-    where estado = 'completada' or estado = 'en progreso'
+        ct.estado
+    from cotizaciones.tareas ct
+	join cotizaciones.cotizaciones on ct.id_cotizacion=ct.id_cotizacion
+    where ct.estado = 'completada' or ct.estado = 'en progreso' and 
+	  (@fecha_inicio is null or ct.fecha_inicio >= @fecha_inicio) 
+        and 
+        (@fecha_fin is null or ct.fecha_inicio <= @fecha_fin)
+
+
     order by 
         fecha_inicio asc
 );
 go
 -----------------------------------Top 10 de clientes con mayores ventas-------------------------------
-create function dbo.top_clientes()
+create function dbo.top_clientes(
+ @fecha_inicio date = null,
+    @fecha_fin date = null
+
+)
 returns table
 as
 return
@@ -32,13 +45,21 @@ return
         sum(f.total) as monto
     from facturación.facturas f
     inner join  clientes.cliente c on f.id_cliente = c.cedula
-    where f.estado = 'aprobada' 
+
+    where f.estado = 'aprobada' and 
+	(@fecha_inicio is null or f.fecha_factura >= @fecha_inicio) 
+        and 
+        (@fecha_fin is null or f.fecha_factura <= @fecha_fin)
     group by  c.nombre
     order by  monto desc
 );
 go
+
 -----------------------------------Cantidad de clientes por zona y monto ventas por zona------------------------
-create function dbo.clientes_zona()
+create  function dbo.clientes_zona(
+@fecha_inicio date = null,
+    @fecha_fin date = null
+)
 returns table
 as
 return
@@ -46,14 +67,20 @@ return
     select c.zona, count(distinct c.cedula) as cantidad_clientes, sum(f.total) as monto_total_ventas
     from  clientes.cliente c
     left join  facturación.facturas f on c.cedula = f.id_cliente
-    where  f.estado = 'aprobada'
+    where  f.estado = 'aprobada'and 
+	 (@fecha_inicio is null or f.fecha_factura >= @fecha_inicio) 
+        and 
+        (@fecha_fin is null or f.fecha_factura <= @fecha_fin)
     group by  c.zona
 );
 go
 
 ---------------------------------------Casos por cotizacion y facturas 
 
-create function dbo.casos_cotizacion_factura()
+create function dbo.casos_cotizacion_factura(
+ @fecha_inicio date = null,
+    @fecha_fin date = null
+)
 returns table
 as
 return
@@ -64,7 +91,10 @@ return
         count(c.id_caso) as cantidad_casos
     from 
         registro_caso.casos c
-    where c.id_factura IS NULL
+    where c.id_factura IS NULL and
+        (@fecha_inicio is null or c.fecha_creacion >= @fecha_inicio) 
+        and 
+        (@fecha_fin is null or c.fecha_creacion <= @fecha_fin)
     group by  year(c.fecha_creacion), month(c.fecha_creacion)
     
     union all
@@ -74,8 +104,11 @@ return
         'factura' as tipo,
         count(c.id_caso) as cantidad_casos
     from registro_caso.casos c
-    where c.id_cotizacion  IS NULL
+    where c.id_cotizacion  IS NULL and 
+	  (@fecha_inicio is null or c.fecha_creacion >= @fecha_inicio) 
+        and 
+        (@fecha_fin is null or c.fecha_creacion <= @fecha_fin)
     group by year(c.fecha_creacion), month(c.fecha_creacion)
 );
-select * from casos_cotizacion_factura()
+
 
