3beb4449c1c581399f5ec852eebd3bf152c44911
ELDERPEREZ <colegioempresarial20@gmail.com>
2024-11-17 00:38:18 -0600

    .

--- Codigo/Pages/ReportesE/PlanillaAnio.cshtml.cs
+++ Codigo/Pages/ReportesE/PlanillaAnio.cshtml.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Threading.Tasks;
@@ -9,70 +10,69 @@ using proyecto1bases.Models;
 
 namespace proyecto1bases.Pages
 {
-   
-   
-   public class MAño : PageModel
-{
-    private readonly string _connectionString;
-
-    public List<PlanillaDataAnio> Planilla { get; set; } = new List<PlanillaDataAnio>();
-    public int? AnioInicio { get; set; }
-    public string? MesInicio { get; set; }
-    public int? AnioFin { get; set; }
-    public string? MesFin { get; set; }
-
-    public MAño(IConfiguration configuration)
+    public class MAño : PageModel
     {
-        _connectionString = configuration.GetConnectionString("DefaultConnection");
-    }
+        private readonly string _connectionString;
 
-    public async Task<IActionResult> OnGetAsync(int? anioInicio, string? mesInicio, int? anioFin, string? mesFin)
-    {
-        AnioInicio = anioInicio;
-        MesInicio = mesInicio;
-        AnioFin = anioFin;
-        MesFin = mesFin;
+        public List<PlanillaDataAnio> Planilla { get; set; } = new List<PlanillaDataAnio>();
+        public int? AñoInicio { get; set; }
+        public string? MesInicio { get; set; }
+        public int? AñoFin { get; set; }
+        public string? MesFin { get; set; }
 
-        using (var connection = new SqlConnection(_connectionString))
+        public MAño(IConfiguration configuration)
         {
-            await connection.OpenAsync();
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
 
-            // Consulta SQL con parámetros para los años y meses
-            var query = "SELECT * FROM usuarios.PlanillaMesse(@anioInicio, @mesInicio, @anioFin, @mesFin)";
 
-            using (var command = new SqlCommand(query, connection))
+        public async Task<IActionResult> OnGetAsync(int? añoInicio, string? mesInicio, int? añoFin, string? mesFin)
+        {  
+            // Asignar los valores a las propiedades del modelo
+            AñoInicio = añoInicio;
+            MesInicio = mesInicio;
+            AñoFin = añoFin;
+            MesFin = mesFin;
+              Console.WriteLine($"Año Inicio: {añoInicio}, Mes Inicio: {mesInicio}, Año Fin: {añoFin}, Mes Fin: {mesFin}");
+
+            // Consultar los datos de la base de datos
+            using (var connection = new SqlConnection(_connectionString))
             {
-                command.Parameters.AddWithValue("@anioInicio", (object)AnioInicio ?? DBNull.Value);
-                command.Parameters.AddWithValue("@mesInicio", (object)MesInicio ?? DBNull.Value);
-                command.Parameters.AddWithValue("@anioFin", (object)AnioFin ?? DBNull.Value);
-                command.Parameters.AddWithValue("@mesFin", (object)MesFin ?? DBNull.Value);
+                await connection.OpenAsync();
 
-                using (var reader = await command.ExecuteReaderAsync())
+                var query = "SELECT * FROM [usuarios].[PlanillaMesse](@año_inicio, @mes_inicio, @año_fin, @mes_fin)";
+                using (var command = new SqlCommand(query, connection))
                 {
-                    while (await reader.ReadAsync())
+                    command.Parameters.AddWithValue("@año_inicio", (object)añoInicio ?? DBNull.Value);
+                    command.Parameters.AddWithValue("@mes_inicio", (object)mesInicio ?? DBNull.Value);
+                    command.Parameters.AddWithValue("@año_fin", (object)añoFin ?? DBNull.Value);
+                    command.Parameters.AddWithValue("@mes_fin", (object)mesFin ?? DBNull.Value);
+
+                    using (var reader = await command.ExecuteReaderAsync())
                     {
-                        Planilla.Add(new PlanillaDataAnio
+                        while (await reader.ReadAsync())
                         {
-                            Año = reader["año"].ToString(),
-                            Mes = reader["mes"].ToString(),
-                            TotalSalario = reader["total_salario"] != DBNull.Value ? decimal.Parse(reader["total_salario"].ToString()) : 0
-                        });
+                            Planilla.Add(new PlanillaDataAnio
+                            {
+                                Año = reader["año"]?.ToString() ?? string.Empty,
+                                Mes = reader["mes"]?.ToString() ?? string.Empty,
+                                TotalSalario = reader["total_salario"] != DBNull.Value 
+                                    ? Convert.ToDecimal(reader["total_salario"]) 
+                                    : 0
+                            });
+                        }
                     }
                 }
             }
+
+            return Page();
         }
-        return Page();
     }
-}
 
-public class PlanillaDataAnio
-{
-    public string Año { get; set; }
-    public string Mes { get; set; }
-    public decimal TotalSalario { get; set; }
-}
-
-
-
-   
+    public class PlanillaDataAnio
+    {
+        public string Año { get; set; }
+        public string Mes { get; set; }
+        public decimal TotalSalario { get; set; }
+    }
 }
